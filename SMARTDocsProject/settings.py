ce945f96c509fc0c42aabaecaab974cc0130e2d7
Rustem Yamikov <jarustem@gmail.com>
2018-07-18 22:14:11 +0300

    fix

--- SMARTDocsProject/settings.py
+++ SMARTDocsProject/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../main/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

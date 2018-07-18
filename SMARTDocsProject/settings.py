af430a94f4bfd081aa42feaa3080842909f5c0e2
Rustem Yamikov <jarustem@gmail.com>
2018-07-18 22:04:45 +0300

    fix

--- SMARTDocsProject/settings.py
+++ SMARTDocsProject/settings.py
@@ -127,16 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-_PATH = BASE_DIR
+#_PATH = BASE_DIR
 
-MEDIA_ROOT = os.path.join(_PATH, 'files', 'media')
-MEDIA_URL = '/media/'
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../main/static')
+#MEDIA_ROOT = os.path.join(_PATH, 'files', 'media')
+#MEDIA_URL = '/media/'
 
 #STATIC_ROOT = os.path.join(_PATH, 'react_app', 'static')
 #STATIC_URL = '/static/'
@@ -147,3 +141,15 @@ STATICFILES_DIRS = (
 #    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
